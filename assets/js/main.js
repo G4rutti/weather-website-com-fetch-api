021f64a7ca2037be56769d9d67b23d4e915c56fb
G4rutti <davigarutti5@gmail.com>
2023-02-19 11:21:54 -0300

    erro cors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,32 @@
 mainFunc = async() => {
-        const data = await fetchTempo(document.getElementById("cidade").value)
-        const dataTraducao = await fetchTraducao(data["current"]["condition"]["text"])
-        console.log(dataTraducao)
+        const data = await fetchTempo(document.getElementById("cidades").value,document.getElementById("estado").value)
         if(data){
-            document.getElementById("cidade").value = ""
-            const forecast = data["forecast"]["forecastday"]
-            console.log(forecast)
-            const divPrincipal = document.getElementById("infoCidade")
-            divPrincipal.innerHTML = `
-                <h2>${data["location"]["name"]} - ${data["current"]["temp_c"]}C°</h2>
-                <ul class="condicoesAtuais" id="vasco">
-                <li>
-                    <img src="${data["current"]["condition"]["icon"]}" alt="">
-                </li>
-                <li id="condicoesAtuais">
-                    ${dataTraducao["matches"][0]["translation"]}
-                </li>
-                </ul>
-                <ul class="minMax">
-                    <li id="minima">
-                        <i class="fa-solid fa-arrow-down"></i>
-                        ${data["forecast"]["forecastday"][0]["day"]["mintemp_c"]}C°
-                    </li>
-                    <li class="maxima">
-                        <i class="fa-solid fa-arrow-up"></i>
-                        ${data["forecast"]["forecastday"][0]["day"]["maxtemp_c"]}C°
-                    </li>
-                </ul>
-            `
+            console.log(data)
+            // document.getElementById("cidade").value = ""
+            // const forecast = data["forecast"]["forecastday"]
+            // console.log(forecast)
+            // const divPrincipal = document.getElementById("infoCidade")
+            // divPrincipal.innerHTML = `
+            //     <h2>${data["location"]["name"]} - ${data["current"]["temp_c"]}C°</h2>
+            //     <ul class="condicoesAtuais" id="vasco">
+            //     <li>
+            //         <img src="${data["current"]["condition"]["icon"]}" alt="">
+            //     </li>
+            //     <li id="condicoesAtuais">
+            //         ${dataTraducao["matches"][0]["translation"]}
+            //     </li>
+            //     </ul>
+            //     <ul class="minMax">
+            //         <li id="minima">
+            //             <i class="fa-solid fa-arrow-down"></i>
+            //             ${data["forecast"]["forecastday"][0]["day"]["mintemp_c"]}C°
+            //         </li>
+            //         <li class="maxima">
+            //             <i class="fa-solid fa-arrow-up"></i>
+            //             ${data["forecast"]["forecastday"][0]["day"]["maxtemp_c"]}C°
+            //         </li>
+            //     </ul>
+            // `
             
         }
         else{
@@ -36,8 +35,8 @@ mainFunc = async() => {
 }
 
 //Comunicação com a API
-const fetchTempo = async(cidade) => {
-    const APIResponse = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=663290ca46664c738af234031221806&q=${cidade}&days=1&aqi=no&alerts=no
+const fetchTempo = async(cidade, estado) => {
+    const APIResponse = await fetch(`https://api.hgbrasil.com/weather?key=a623e753&city_name=${cidade},${estado}
     `)
     if(APIResponse.status === 200){
         const data = await APIResponse.json()

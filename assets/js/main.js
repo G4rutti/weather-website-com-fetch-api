95b41222f6f025393c5c58d9874d266a05dae08a
G4rutti <davigarutti5@gmail.com>
2023-02-19 12:51:09 -0300

    só falta a imagem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,30 @@
 mainFunc = async() => {
         const data = await fetchTempo(document.getElementById("cidades").value, document.getElementById("estado").value)
         if(data){
-            console.log(data)
-            // document.getElementById("cidade").value = ""
-            // const forecast = data["forecast"]["forecastday"]
-            // console.log(forecast)
-            // const divPrincipal = document.getElementById("infoCidade")
-            // divPrincipal.innerHTML = `
-            //     <h2>${data["location"]["name"]} - ${data["current"]["temp_c"]}C°</h2>
-            //     <ul class="condicoesAtuais" id="vasco">
-            //     <li>
-            //         <img src="${data["current"]["condition"]["icon"]}" alt="">
-            //     </li>
-            //     <li id="condicoesAtuais">
-            //         ${dataTraducao["matches"][0]["translation"]}
-            //     </li>
-            //     </ul>
-            //     <ul class="minMax">
-            //         <li id="minima">
-            //             <i class="fa-solid fa-arrow-down"></i>
-            //             ${data["forecast"]["forecastday"][0]["day"]["mintemp_c"]}C°
-            //         </li>
-            //         <li class="maxima">
-            //             <i class="fa-solid fa-arrow-up"></i>
-            //             ${data["forecast"]["forecastday"][0]["day"]["maxtemp_c"]}C°
-            //         </li>
-            //     </ul>
-            // `
+            console.log(data["results"])
+            const forecast = data["results"]["forecast"][0]
+            const divPrincipal = document.getElementById("infoCidade")
+            divPrincipal.innerHTML = `
+                <h2>${data["results"]["city_name"]} - ${data["results"]["temp"]}C°</h2>
+                <ul class="condicoesAtuais" id="vasco">
+                <li>
+                    
+                </li>
+                <li id="condicoesAtuais">
+                    <p>${data["results"]["description"]}</p>
+                </li>
+                </ul>
+                <ul class="minMax">
+                    <li id="minima">
+                        <i class="fa-solid fa-arrow-down"></i>
+                        ${forecast["min"]}C°
+                    </li>
+                    <li class="maxima">
+                        <i class="fa-solid fa-arrow-up"></i>
+                        ${forecast["max"]}C°
+                    </li>
+                </ul>
+            `
             
         }
         else{
